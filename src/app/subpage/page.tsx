316908841834a7837f1232050645955400f3d3e2
Jaroslav Vercinskij <jaroslav-vercinskij@outlook.com>
2023-12-16 19:51:41 +0200

    Initial.

---
+++ src/app/subpage/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Subpage | nextjs-metadata",
+  description: "This is metadata of subpage.",
+};
+
+const Subpage: React.FC = () => {
+  return (
+    <>
+      <p>I am a subpage.</p>
+      <a href="/">Return to homepage</a>
+    </>
+  );
+};
+
+export default Subpage;
